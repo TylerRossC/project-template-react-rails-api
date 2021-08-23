db54722156c2182add324885bf54e3954f80293c
tyler <tylercharawell@gmail.com>
2021-08-23 16:27:25 -0700

    Reconfigured files for deployment to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :blocks
-  resources :users
+  namespace :api do
   
-  get '/me', to: 'users#show'
+    resources :blocks
+    resources :users
+    
+    get '/me', to: 'users#show'
+    
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
   
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
